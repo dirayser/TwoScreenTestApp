ffe7ef4d0f5463dfedaad7a7b0d67eb23728e3fa
Dmitriy Dmytriiev <d.dmytriiev@sweet.tv>
2025-04-27 22:49:33 +0300

    Initial setup

---
+++ TwoScreenTestApp/TwoScreenTestApp/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoScreenTestApp
+//
+//  Created by Dmitriy Dmitriyev on 27.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
